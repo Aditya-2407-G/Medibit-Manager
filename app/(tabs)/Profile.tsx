302bb11bb7e5cc417e3c235a3638275d305ef1c3
Aditya-2407-G <adi.gupta2407@gmail.com>
2024-07-05 23:33:08 +0530

    profileUpdate change password

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,44 +1,174 @@
-import { router } from "expo-router";
+import { View, Text, TouchableOpacity, Image, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { View, Image, FlatList, TouchableOpacity, Text } from "react-native";
-
-import { icons } from "../../constants";
-import { siguOut } from "../../lib/appwrite";
-import { useGlobalContext } from "../../context/GlobalContextProvider";
-
-
+import {
+    Button,
+    Modal,
+    PaperProvider,
+    Portal,
+    TextInput,
+} from "react-native-paper";
+import { useGlobalContext } from "@/context/GlobalContextProvider";
+import { changePassword, getAccount, siguOut } from "@/lib/appwrite";
+import { router } from "expo-router";
 
 const Profile = () => {
 
-  const { user, setUser, setIsLogged } = useGlobalContext();
+    const { user, setUser, setIsLogged } = useGlobalContext();
+    const [visible, setVisible] = useState(false);
+    const [oldpassword, setOldPassword] = useState("");
+    const [newpassword, setNewPassword] = useState("");
+    const [confirmpassword, setConfirmPassword] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
 
-  const logout = async () => {
-    await siguOut();
-    setUser(null);
-    setIsLogged(false); 
 
-    router.replace("/Signin");
-  };
 
-  return (
-    <SafeAreaView className="bg-primary h-full">
-      <View className="flex items-center justify-center">
-        <View className="flex items-center justify-center gap-2">
+    const showModel = () => setVisible(true);
+    const hideModel = () => {
+        setVisible(false);
+        setOldPassword("");
+        setNewPassword("");
+        setConfirmPassword("");
+    }
 
-          <Text className="font-psemibold text-lg">{user?.name}</Text>
-        </View>
-        <TouchableOpacity onPress={logout}>
-        <Image
-                source={icons.logout}
-                resizeMode="contain"
-                className="w-6 h-6"
-              />
-          {/* <Text className="text-red-500">Logout</Text> */}
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-    
-  );
+
+    const logout = async () => {
+        await siguOut();
+        setUser(null);
+        setIsLogged(false);
+        router.replace("/Signin");
+    };
+
+    const handlePasswordChange = async () => {
+        if (!oldpassword || !newpassword || !confirmpassword) {
+            alert("Please fill all fields");
+            return;
+        }
+        if (newpassword !== confirmpassword) {
+            alert("Passwords do not match");
+            return;
+        }
+        try {
+            await changePassword(newpassword, oldpassword);
+            Alert.alert("Success", "Password changed successfully");
+            hideModel();
+
+        } catch (error) {
+            Alert.alert("Error", "Password don't match!");
+        }
+    };
+
+
+    useEffect(() => {
+        getAccount().then((res) => {
+            setName(res.name);
+            setEmail(res.email);
+        })
+    })
+
+    return (
+        <PaperProvider>
+            <SafeAreaView className="flex-1 bg-primary p-4">
+
+                <Text className="text-white text-center text-3xl font-bold mb-8">
+                    Your Profile
+                </Text>
+
+                <View className="mt-10">
+
+                    <View className="bg-gray-800 rounded-lg p-6 mb-8">
+                        <Text className="text-white text-center text-xl font-bold mb-2">
+                            {name}
+                        </Text>
+                        <Text className="text-gray-400 text-center text-lg">
+                            {email}
+                        </Text>
+                    </View>
+
+
+                    <Button
+                        icon="account-edit"
+                        mode="contained-tonal"
+                        onPress={showModel}
+                        className="mb-8 mt-10 p-2"
+                        labelStyle={{ fontSize: 16 }}                        
+                    >
+                        Change Password
+                    </Button>
+
+                    <Button
+                        icon="logout"
+                        mode="contained-tonal"
+                        onPress={logout}
+                        className="p-2"
+                    >
+                        <Text>Logout</Text>
+                    </Button>
+                </View>
+
+                <Portal>
+                    <Modal
+                        visible={visible}
+                        onDismiss={hideModel}
+                        contentContainerStyle={{
+                            backgroundColor: "#1E1E1E",
+                            height: "70%",
+                            width: "80%",
+                            borderRadius: 20,
+                            alignSelf: "center",
+                        }}
+                    >
+                        <Text className="text-white text-2xl font-bold mb-10 text-center">
+                            Change Password
+                        </Text>
+                        <View className="space-y-7 px-4">
+                            <TextInput
+                                placeholder="Current Password"
+                                secureTextEntry
+                                value={oldpassword}
+                                onChangeText={setOldPassword}
+                                className="bg-gray-800 rounded-lg "
+                                textColor="white"
+                                placeholderTextColor="#9CA3AF"
+                            />
+                            <TextInput
+                                placeholder="New Password"
+                                secureTextEntry
+                                value={newpassword}
+                                textColor="white"
+                                onChangeText={setNewPassword}
+                                className="bg-gray-800 text-white rounded-lg"
+                                placeholderTextColor="#9CA3AF"
+                            />
+                            <TextInput
+                                placeholder="Confirm Password"
+                                secureTextEntry
+                                value={confirmpassword}
+                                textColor="white"
+                                onChangeText={setConfirmPassword}
+                                className="bg-gray-800 text-white rounded-lg"
+                                placeholderTextColor="#9CA3AF"
+                            />
+                        </View>
+                        <TouchableOpacity
+                            onPress={handlePasswordChange}
+                            className="bg-blue-500 py-2  rounded-lg m-10 active:bg-blue-600"
+                        >
+                            <Text className="text-white text-center text-lg font-bold">
+                                Update Password
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={hideModel} className="mt-4">
+                            <Text className="text-gray-400 text-center text-base">
+                                Cancel
+                            </Text>
+                        </TouchableOpacity>
+                    </Modal>
+                </Portal>
+            </SafeAreaView>
+        </PaperProvider>
+    );
 };
 
 export default Profile;
--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,35 @@
-import { Redirect, router } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import { Button, ScrollView, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import CustomButton from "../components/CustomButton";
-import { useGlobalContext } from "../context/GlobalContextProvider"
-import { HelloWave } from "@/components/HelloWave";
+  import { Redirect, router } from "expo-router";
+  import { StatusBar } from "expo-status-bar";
+  import { Button, ScrollView, Text, View } from "react-native";
+  import { SafeAreaView } from "react-native-safe-area-context";
+  import CustomButton from "../components/CustomButton";
+  import { useGlobalContext } from "../context/GlobalContextProvider"
+  import { HelloWave } from "@/components/HelloWave";
 
-export default function Index() {
+  export default function Index() {
 
-  const {isLoading, isLogged} = useGlobalContext();
+    const {isLoading, isLogged} = useGlobalContext();
 
-  if(!isLoading && isLogged){
-    return <Redirect href="/Home" />
+    if(!isLoading && isLogged){
+      return <Redirect href="/Home" />
+    }
+    return (
+      <SafeAreaView className="bg-primary h-full">
+          <ScrollView contentContainerStyle= {{height: '100%'}}>
+
+              <StatusBar backgroundColor="#161622" style="light" />
+
+
+                  <View className="flex items-center justify-center min-h-[85vh]">
+              <HelloWave />
+                      <Text className=" text-center text-white font-bold text-3xl p-10 mb-20">Welcome to Medibit Manager!</Text>
+                      <CustomButton title="Sign In" handlePress={() => router.push("/Signin")} containerStyles="w-full mb-10" textStyles={undefined} isLoading={undefined} />
+                      <CustomButton title="Sign Up" handlePress={() => router.push("/Signup")} containerStyles="w-full" textStyles={undefined} isLoading={undefined} />
+                  </View>
+                  
+
+
+          </ScrollView>
+      </SafeAreaView>
+    );
   }
-  return (
-    <SafeAreaView className="bg-primary h-full">
-        <ScrollView contentContainerStyle= {{height: '100%'}}>
-
-            <StatusBar backgroundColor="#161622" style="light" />
-
-
-                <View className="flex items-center justify-center min-h-[85vh]">
-            <HelloWave />
-                    <Text className=" text-center text-white font-bold text-3xl p-10 mb-20">Welcome to Medibit Manager!</Text>
-                    <CustomButton title="Sign In" handlePress={() => router.push("/Signin")} containerStyles="w-full mb-10" textStyles={undefined} isLoading={undefined} />
-                    <CustomButton title="Sign Up" handlePress={() => router.push("/Signup")} containerStyles="w-full" textStyles={undefined} isLoading={undefined} />
-                </View>
-                
-
-
-        </ScrollView>
-    </SafeAreaView>
-  );
-}
